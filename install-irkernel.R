a4c24cd9fde10f7adb1f9be91353d31756920f67
Serhat Cevikel <serhatcevikel@yahoo.com>
2018-03-16 01:05:07 +0300

    commit

--- install-irkernel.R
+++ install-irkernel.R
@@ -1,6 +1,6 @@
 #mirid <- match("tr", getCRANmirrors()[,8])
 #chooseCRANmirror(graphics=FALSE, ind=mirid)
-chooseCRANmirror(graphics=FALSE, ind=1)
+chooseCRANmirror(graphics=FALSE, ind=2)
 
 install.packages("devtools")
 devtools::install_github("IRkernel/IRkernel")
